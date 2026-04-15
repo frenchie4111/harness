aab4f77db0b38c71f79da3f14291e892c47a08fd
Mike Lyons <mdl0394@gmail.com>
2026-04-15 13:26:07 -0600

    enable-electron-sandbox (squashed)

--- src/main/index.ts
+++ src/main/index.ts
@@ -316,7 +316,7 @@ function createWindow(): BrowserWindow {
       preload: join(__dirname, '../preload/index.js'),
       contextIsolation: true,
       nodeIntegration: false,
-      sandbox: false
+      sandbox: true
     }
   })
 
